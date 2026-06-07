db2139f26aa4251df39e614460e43accbfdc3ae5
Gustavo Ledesma <gustavo.lede@gmail.com>
2026-06-07 19:23:36 -0300

    Distribución de Carpetas de objetos y creación del repository para pruebas

---
+++ Backend/src/features/objetos/repository/objetos.repository.js
@@ -0,0 +1,19 @@
+//PARA PRUEBAS
+let objetosprestados=[
+  { 
+    id:1,
+    titulo:"taladro",
+    descripción: "taladro de mano",
+    categoria: "Herramientas",
+    estado: "disponible",// no seria un booleano?
+    usuarioId:1
+  },
+  { 
+    id:2,
+    titulo:"destornillador",
+    descripción: "destornillador manual",
+    categoria: "Herramientas",
+    estado: "disponible",// no seria un booleano?
+    usuarioId:2
+  }
+];
