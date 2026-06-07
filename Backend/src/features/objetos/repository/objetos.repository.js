5c4c5b5c6d6a6333731b88971fda589713d9399d
Gustavo Ledesma <gustavo.lede@gmail.com>
2026-06-07 20:41:57 -0300

    funcion del repository
    trae todos los objetos filtrados

--- Backend/src/features/objetos/repository/objetos.repository.js
+++ Backend/src/features/objetos/repository/objetos.repository.js
@@ -1,19 +1,38 @@
-//PARA PRUEBAS
-let objetosprestados=[
-  { 
-    id:1,
-    titulo:"taladro",
-    descripción: "taladro de mano",
-    categoria: "Herramientas",
-    estado: "disponible",// no seria un booleano?
-    usuarioId:1
-  },
-  { 
-    id:2,
-    titulo:"destornillador",
-    descripción: "destornillador manual",
-    categoria: "Herramientas",
-    estado: "disponible",// no seria un booleano?
-    usuarioId:2
-  }
-];
+const objetos = require('./src/objetos.js').objetos;
+
+exports.getAllobjetsRepository = async () =>{
+    
+    try {
+         console.log("REPOSITORY - getAllObjetsRepository ")
+         return await JSON.stringify(objetos)
+    } catch (error) {
+        console.log("Error en getAllObjetsRepository ", error)
+    }
+   
+}
+
+
+exports.getObjetsFilteredRepository = async (lenguaje, orderby) =>{
+
+    const filtrado = objetos.filter(
+        objetos => objetos.id.toLocaleLowerCase() === lenguaje.toLocaleLowerCase()
+    )
+
+    if(filtrado.length === 0){
+        return []
+    }
+
+    if(orderby === 'arriba'){
+        return filtrado.sort(
+            (a,b) => b.cantidadAlumnos - a.cantidadAlumnos
+        )
+    }
+    else if (orderby === 'abajo'){
+        return filtrado.sort(
+            (a,b) => a.cantidadAlumnos - b.cantidadAlumnos
+        )
+    }else{
+        return filtrado
+    }
+
+}
---
+++ Backend/src/features/objetos/src/objetos.js
@@ -0,0 +1,23 @@
+//PARA PRUEBAS
+let objetosprestados=[
+  { 
+    id:1,
+    titulo:"taladro",
+    descripción: "taladro de mano",
+    categoria: "Herramientas",
+    estado: "disponible",// no seria un booleano?
+    duenioId:1,
+    fechacreacion: "12/12/2012"
+  },
+  { 
+    id:2,
+    titulo:"destornillador",
+    descripción: "destornillador manual",
+    categoria: "Herramientas",
+    estado: "disponible",// no seria un booleano?
+    duenioId:2,
+    fechacreacion: "12/12/2012"
+  }
+];
+
+module.exports={objetosprestados}
