ea9c9abca8e791cc858fe1490126fe83a31f8aba
GusLed1870 <gustavo.lede@gmail.com>
2026-06-11 11:12:07 -0300

    Update objetos.repository.js

--- Backend/src/features/objetos/objetos.repository.js
+++ Backend/src/features/objetos/objetos.repository.js
@@ -56,3 +56,38 @@ exports.getObjetsFilteredRepository = async (lenguaje, orderby) =>{
     }
 
 }
+
+
+
+
+
+// CREATE
+exports.crearObjetoRepository = async (datosObjeto) => {
+    try {
+        console.log("REPOSITORY - crearObjeto", datosObjeto)
+        const objeto = new Objeto(datosObjeto)
+        return await objeto.save()
+    } catch (error) {
+        throw error
+    }
+}
+
+//UPDATE
+exports.editarObjetoRepository = async (id, objetoActualizado) => {
+    try {
+        console.log("REPOSITORY - editarObjetoRepository - id:", id, "- objetoActualizado:", objetoActualizado)
+        return await Objeto.findByIdAndUpdate(id, objetoActualizado, { new: true })
+    } catch (error) {
+        throw error
+    }
+}
+
+//DELETE
+exports.eliminarObjetoRepository = async (id) => {
+    try {
+        console.log("REPOSITORY - eliminarObjetoRepository - id:", id)
+        return await Objeto.findByIdAndDelete(id)
+    } catch (error) {
+        throw error
+    }
+}
